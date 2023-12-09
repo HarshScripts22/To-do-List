432bbb34ac59da7e9aeda3e87b35346b23d594b6
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2023-12-10 06:06:36 +0700

    progress 01

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const todo = document.querySelector(".main");
+const todo = document.querySelector("#output");
 const btn = document.querySelector(".btn");
 let inputElement = document.getElementById("task");
+
 btn.addEventListener("click", function (e) {
   let value = inputElement.value;
+  let h2 = document.createElement("h2");
+  h2.textContent = value;
   let output = document.getElementById("output");
-  output.innerHTML = `<h2>${value}</h2>`;
+  let tick = document.createElement("img");
+
+  tick.setAttribute("src", "./checkbox.png");
+  tick.setAttribute("alt", "error");
+
+  tick.style.height = "40px";
+  tick.style.width = "40px";
+
+  output.appendChild(tick);
+  output.appendChild(h2);
+  inputElement.value = "";
 });
