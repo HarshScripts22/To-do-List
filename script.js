0151ad3f776c99b425f7424720be32a981361333
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2023-12-12 05:53:44 +0700

    progress02_done

--- script.js
+++ script.js
@@ -9,13 +9,23 @@ btn.addEventListener("click", function (e) {
   let output = document.getElementById("output");
   let tick = document.createElement("img");
 
-  tick.setAttribute("src", "./checkbox.png");
+  tick.setAttribute("src", "./empty.png");
   tick.setAttribute("alt", "error");
 
-  tick.style.height = "40px";
-  tick.style.width = "40px";
+  tick.classList.add("tick");
+  // tick.style.height = "30px";
+  // tick.style.width = "30px";
 
-  output.appendChild(tick);
-  output.appendChild(h2);
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("content");
+  output.appendChild(newDiv);
+  newDiv.appendChild(tick);
+  newDiv.appendChild(h2);
   inputElement.value = "";
+});
+output.addEventListener("click", function (e) {
+  if (e.target.classList.contains("tick")) {
+    // console.log("hello world");
+    e.target.setAttribute("src", "./checkbox.png");
+  }
 });
