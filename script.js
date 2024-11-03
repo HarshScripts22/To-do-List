1bea6f893bfb6305c4a7439fdac8173674a3437f
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2024-11-03 14:10:40 +0530

    make changes in css and js part

--- script.js
+++ script.js
@@ -1,59 +1,41 @@
-const todo = document.querySelector("#output");
-const btn = document.querySelector(".btn");
-let inputElement = document.getElementById("task");
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
 
-btn.addEventListener("click", function (e) {
-  let value = inputElement.value;
-  let h2 = document.createElement("h2");
-
-  h2.textContent = value;
-  h2.id = "heading";
-  let output = document.getElementById("output");
-  let tick = document.createElement("img");
-  tick.classList.add("tick");
-  tick.setAttribute("src", "./empty.png");
-  tick.setAttribute("alt", "error");
-
-  let del = document.createElement("div");
-  del.classList.add("delete");
-  let cross = document.createElement("img");
-  cross.classList.add("cut");
-  cross.setAttribute("src", "./bin.png");
-  cross.setAttribute("alt", "error");
-
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("content");
-
-  output.appendChild(newDiv);
-  del.appendChild(cross);
-  newDiv.appendChild(tick);
-  newDiv.appendChild(h2);
-  newDiv.appendChild(del);
-  inputElement.value = "";
-});
-output.addEventListener("click", function (e) {
-  if (e.target.classList.contains("untick")) {
-    e.target.setAttribute("src", "./empty.png");
-    e.target.classList.remove("untick");
-    let y = e.target.parentNode;
-    y.childNodes[1].style.textDecoration = "none";
-  } else if (e.target.classList.contains("tick")) {
-    e.target.setAttribute("src", "./checkbox.png");
-    e.target.classList.add("untick");
-    let y = e.target.parentNode;
-    y.childNodes[1].style.textDecoration = "line-through";
-  } else if (e.target.classList.contains("cut")) {
-    let divtoremove = e.target.parentNode.parentNode;
-    divtoremove.remove();
+function addTask() {
+  if (inputbox.value === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputbox.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
-  // save();
-});
-// function save() {
-//   localStorage.setItem("data", content.innerHTML);
-// }
+  inputbox.value = "";
+  savedata();
+}
 
-// function show() {
-//   todo.innerHTML = localStorage.getItem("data");
-// }
+listcontainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      savedata();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      savedata();
+    }
+  },
+  false
+);
 
-// show();
+function savedata() {
+  localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+function showlist() {
+  listcontainer.innerHTML = localStorage.getItem("data");
+}
+
+showlist();
